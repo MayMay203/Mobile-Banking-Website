58735d0087578b09ee01bbadfc76dc2cc9bb3c22
Nhung <hongnhung16052003@gmail.com>
2024-07-27 15:53:31 +0700

    build UI of the feedback section

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,3 +7,23 @@ navarLink.forEach(link => {
         e.target.classList.add('navbar__link--active')
     })
 })
+
+const feedbackDots = document.querySelectorAll('.feedback__dot')
+const feedbackLeft = document.querySelectorAll('.feedback-left')
+const feedbackRight = document.querySelectorAll('.feedback-right')
+feedbackDots.forEach((dot) => {
+    dot.addEventListener('click', e => {
+        feedbackDots.forEach(dot => {
+            dot.classList.remove('feedback__dot--active')
+        })
+        e.target.classList.add('feedback__dot--active')
+        index = e.target.dataset.index
+        feedbackLeft.forEach(feedback => {
+            feedback.style.transform = `translateX(${-(index - 1) * 100}%)`
+        })
+        feedbackRight.forEach(feedback => {
+            feedback.style.transform = `translateX(${-(index - 1) * 100}%)`
+        })
+    })
+})
+
